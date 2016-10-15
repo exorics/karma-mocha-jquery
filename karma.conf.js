25b4f9fe1ecc57540a497ffdd132e1e9fd3af96f
exorics <sixdayj@gmail.com>
2016-10-16 00:05:12 +0800

    调整chai jquery为web方式集成

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha','seajs','chai'],
+    frameworks: ['mocha','seajs','chai','fixture'],
 
     plugins: [
         'karma-mocha',
@@ -19,7 +19,8 @@ module.exports = function(config) {
         'karma-chrome-launcher',
         'karma-coverage',
         'karma-mocha-reporter',
-        'karma-chai-jquery'
+        'karma-fixture',
+        'karma-html2js-preprocessor'
     ],
 
     // list of files / patterns to load in the browser
@@ -37,7 +38,9 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'src/**/*.js': 'coverage'
+        'src/**/*.js': 'coverage',
+        'tests/**/*.html' : ['html2js']
+
     },
 
 
